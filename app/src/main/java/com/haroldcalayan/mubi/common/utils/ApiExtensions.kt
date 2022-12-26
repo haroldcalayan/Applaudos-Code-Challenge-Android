6c6cad7fc513debd677ee8052071141c87c6e1bb
Harold Calayan <haroldcalayan@gmail.com>
2022-12-27 01:49:31 +0800

    :sparkles: Create utility classes

--- app/src/main/java/com/haroldcalayan/mubi/common/utils/ApiExtensions.kt
+++ app/src/main/java/com/haroldcalayan/mubi/common/utils/ApiExtensions.kt
@@ -1,13 +1,23 @@
-package com.haroldcalayan.mubi.util
+package com.haroldcalayan.mubi.common.utils
 
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
-inline fun <reified T>provideApi(apiUrl: String, httpClient: OkHttpClient): T {
+inline fun <reified T>provideGSONApi(apiUrl: String, httpClient: OkHttpClient): T {
     return Retrofit.Builder()
         .baseUrl(apiUrl)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient)
+        .build()
+        .create(T::class.java)
+}
+
+inline fun <reified T>provideMoshiApi(apiUrl: String, httpClient: OkHttpClient): T {
+    return Retrofit.Builder()
+        .baseUrl(apiUrl)
+        .addConverterFactory(MoshiConverterFactory.create().asLenient())
         .client(httpClient)
         .build()
         .create(T::class.java)
--- app/src/main/java/com/haroldcalayan/mubi/common/utils/ViewExtensions.kt
+++ app/src/main/java/com/haroldcalayan/mubi/common/utils/ViewExtensions.kt
@@ -1,4 +1,4 @@
-package com.haroldcalayan.conversionrates.common.utils
+package com.haroldcalayan.mubi.common.utils
 
 import android.app.Activity
 import android.content.Context
