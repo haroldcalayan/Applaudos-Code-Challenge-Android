3b934786ec214e83eb54f1048ebdbdcec46bff03
Harold Calayan <haroldcalayan@gmail.com>
2022-12-27 01:49:46 +0800

    :sparkles: Create constant file

--- app/src/main/java/com/haroldcalayan/mubi/common/Constants.kt
+++ app/src/main/java/com/haroldcalayan/mubi/common/Constants.kt
@@ -1,13 +1,13 @@
-package com.heroappsdev.mubiapp.common
+package com.haroldcalayan.mubi.common
 
 object Constants {
-
-    const val BASE_URL = "https://api.themoviedb.org/"
-    const val API_KEY = "ecdf8b522ff5e14309092bbef4706f06"
-    const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
     const val CATEGORY_POPULAR = "Popular"
     const val CATEGORY_TOP_RATED = "Top Rated"
     const val CATEGORY_ON_TV = "On TV"
     const val CATEGORY_AIRING_TODAY = "Airing Today"
+
+    const val ONE_SECOND = 1000L
+    const val ONE_MINUTE = 60 * ONE_SECOND
+    const val SPLASH_SCREEN_LIFE = 3 * ONE_SECOND
 }
