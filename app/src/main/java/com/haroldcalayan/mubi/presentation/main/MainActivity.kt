467b39838ca5a656a0b73e0c66d274d1ae64cb23
Harold Calayan <haroldcalayan@gmail.com>
2022-12-27 03:04:43 +0800

    :sparkles: Add toolbar icons (search and profile)

--- app/src/main/java/com/haroldcalayan/mubi/presentation/main/MainActivity.kt
+++ app/src/main/java/com/haroldcalayan/mubi/presentation/main/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.haroldcalayan.mubi.R
+import com.haroldcalayan.mubi.common.ui.theme.MubiTheme
+import com.haroldcalayan.mubi.common.utils.showToastShort
 import com.haroldcalayan.mubi.presentation.main.movie_details.MovieDetailsScreen
 import com.haroldcalayan.mubi.presentation.main.movie_list.MovieListScreen
 import com.haroldcalayan.mubi.presentation.main.tv_details.TVDetailsScreen
-import com.haroldcalayan.mubi.common.ui.theme.MubiTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,31 +30,55 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MubiTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = Screen.MovieListScreen.route
-                    ) {
-                        composable(
-                            route = Screen.MovieListScreen.route
-                        ) {
-                            MovieListScreen(navController)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "TV Shows")
+                            },
+                            backgroundColor = colorResource(id = R.color.primaryDark),
+                            contentColor = colorResource(id = R.color.white),
+                            elevation = 12.dp,
+                            actions = {
+                                IconButton(onClick = { /*TODO*/ }) {
+                                    Icon(Icons.Rounded.Search, "Search")
+                                }
+                                IconButton(onClick = { openProfile() }) {
+                                    Icon(Icons.Rounded.Person, "Profile")
+                                }
+                            }
+                        )
+                    }, content = {
+                        Surface(color = MaterialTheme.colors.background) {
+                            val navController = rememberNavController()
+                            NavHost(
+                                navController = navController,
+                                startDestination = Screen.MovieListScreen.route
+                            ) {
+                                composable(
+                                    route = Screen.MovieListScreen.route
+                                ) {
+                                    MovieListScreen(navController)
+                                }
+                                composable(
+                                    route = Screen.MovieDetailScreen.route + "/{movieId}"
+                                ) {
+                                    MovieDetailsScreen(navController)
+                                }
+                                composable(
+                                    route = Screen.TVDetailScreen.route + "/{tvId}"
+                                ) {
+                                    TVDetailsScreen(navController)
+                                }
+                            }
                         }
-                        composable(
-                            route = Screen.MovieDetailScreen.route + "/{movieId}"
-                        ) {
-                            MovieDetailsScreen(navController)
-                        }
-                        composable(
-                            route = Screen.TVDetailScreen.route + "/{tvId}"
-                        ) {
-                            TVDetailsScreen(navController)
-                        }
-                    }
-                }
+                    })
             }
         }
+    }
+
+    private fun openProfile() {
+        showToastShort("Open Profile")
     }
 }
 
