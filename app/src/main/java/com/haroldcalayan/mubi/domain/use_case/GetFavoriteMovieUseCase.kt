777924d685db99812c0c2f24fe305a95fcaab00d
Harold Calayan <haroldcalayan@gmail.com>
2022-12-28 00:39:27 +0800

    :sparkles: Create favorite movie use case

---
+++ app/src/main/java/com/haroldcalayan/mubi/domain/use_case/GetFavoriteMovieUseCase.kt
@@ -0,0 +1,26 @@
+package com.haroldcalayan.mubi.domain.use_case
+
+import com.haroldcalayan.mubi.common.Response
+import com.haroldcalayan.mubi.data.repository.MovieRepository
+import com.haroldcalayan.mubi.data.source.remote.dto.MoviesDTO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetFavoriteMovieUseCase @Inject constructor(
+    private val repository: MovieRepository
+) {
+    operator fun invoke(accountId: Int, sessionId: String): Flow<Response<MoviesDTO>> = flow {
+        try {
+            emit(Response.Loading())
+            val movies = repository.getFavoriteMovie(accountId = accountId, sessionId = sessionId)
+            emit(Response.Success(movies))
+        } catch (e: HttpException) {
+            emit(Response.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Response.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+}
