44713d6462b1e93b14e8dfc568b1a8ab18f33c74
Harold Calayan <haroldcalayan@gmail.com>
2022-12-27 21:14:59 +0800

    :sparkles: Implement apis in repositories

--- app/src/main/java/com/haroldcalayan/mubi/data/repository/MovieRepository.kt
+++ app/src/main/java/com/haroldcalayan/mubi/data/repository/MovieRepository.kt
@@ -2,9 +2,7 @@ package com.haroldcalayan.mubi.data.repository
 
 import com.haroldcalayan.mubi.common.base.BaseRepository
 import com.haroldcalayan.mubi.data.source.remote.TMDBApi
-import com.haroldcalayan.mubi.data.source.remote.dto.MovieDetailsDTO
-import com.haroldcalayan.mubi.data.source.remote.dto.MoviesDTO
-import com.haroldcalayan.mubi.data.source.remote.dto.TVDetailsDTO
+import com.haroldcalayan.mubi.data.source.remote.dto.*
 
 interface MovieRepository {
     suspend fun getPopularMovies(): MoviesDTO
@@ -13,11 +11,19 @@ interface MovieRepository {
     suspend fun getAiringTodayMovies(): MoviesDTO
     suspend fun getMovieDetails(movieId: Int): MovieDetailsDTO
     suspend fun getTVDetails(tvId: Int): TVDetailsDTO
+    suspend fun getRequestToken(): RequestTokenDTO
+    suspend fun getSeasonDetails(tvId: Int, seasonNumber: Int): SeasonDTO
+    suspend fun searchMovie(query: String): SearchResultDTO
 }
 
 class MovieRepositoryImpl(
     private val api: TMDBApi
 ) : BaseRepository(), MovieRepository {
+
+    override suspend fun getRequestToken(): RequestTokenDTO {
+        return api.getRequestToken()
+    }
+
     override suspend fun getPopularMovies(): MoviesDTO {
         return api.getPopularMovies()
     }
@@ -40,5 +46,13 @@ class MovieRepositoryImpl(
 
     override suspend fun getTVDetails(tvId: Int): TVDetailsDTO {
         return api.getTVDetail(tvId = tvId)
+    }
+
+    override suspend fun getSeasonDetails(tvId: Int, seasonNumber: Int): SeasonDTO {
+        return api.getSeasonDetail(tvId = tvId, seasonNumber = seasonNumber)
+    }
+
+    override suspend fun searchMovie(query: String): SearchResultDTO {
+        return api.getSearch(query = query)
     }
 }
