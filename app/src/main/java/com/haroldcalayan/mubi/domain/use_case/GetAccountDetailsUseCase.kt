528fa20ff783b4af818accd36f795c840a0b2c0e
Harold Calayan <haroldcalayan@gmail.com>
2022-12-28 00:38:54 +0800

    :sparkles: Create account use case

---
+++ app/src/main/java/com/haroldcalayan/mubi/domain/use_case/GetAccountDetailsUseCase.kt
@@ -0,0 +1,26 @@
+package com.haroldcalayan.mubi.domain.use_case
+
+import com.haroldcalayan.mubi.common.Response
+import com.haroldcalayan.mubi.data.repository.MovieRepository
+import com.haroldcalayan.mubi.data.source.remote.dto.AccountDTO
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetAccountDetailsUseCase @Inject constructor(
+    private val repository: MovieRepository
+) {
+    operator fun invoke(sessionId: String): Flow<Response<AccountDTO>> = flow {
+        try {
+            emit(Response.Loading())
+            val data = repository.getAccount(sessionId)
+            emit(Response.Success(data))
+        } catch (e: HttpException) {
+            emit(Response.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Response.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+}
