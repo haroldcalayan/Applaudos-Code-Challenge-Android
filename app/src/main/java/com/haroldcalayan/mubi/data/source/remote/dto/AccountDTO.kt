1c781ab9340565afa8ce3fa17f5ae5e969ca7df5
Harold Calayan <haroldcalayan@gmail.com>
2022-12-28 00:35:56 +0800

    :sparkles: Create account model

---
+++ app/src/main/java/com/haroldcalayan/mubi/data/source/remote/dto/AccountDTO.kt
@@ -0,0 +1,41 @@
+package com.haroldcalayan.mubi.data.source.remote.dto
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class AccountDTO(
+    val avatar: Avatar? = null,
+    val id: Long? = null,
+
+    @Json(name = "iso_639_1")
+    val iso639_1: String? = null,
+
+    @Json(name = "iso_3166_1")
+    val iso3166_1: String? = null,
+
+    val name: String? = null,
+
+    @Json(name = "include_adult")
+    val includeAdult: Boolean? = null,
+
+    val username: String? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class Avatar (
+    val gravatar: Gravatar? = null,
+    val tmdb: Tmdb? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class Gravatar (
+    val hash: String? = null
+)
+
+@JsonClass(generateAdapter = true)
+data class Tmdb (
+    @Json(name = "avatar_path")
+    val avatarPath: Any? = null
+)
+
